9a0842e41cf42cbe0cebcdac41be7ad7dfd4f642
Juan Camilo González <info@juancgonzalez.com>
2019-08-13 08:42:44 -0500

    init about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import DataStore from '../stores/DataStore.js';
 
 export default class Home extends Component {
   render() {
     let page = DataStore.getPageBySlug('sobre');
-
+    console.log(page);
     return (
-      <div>
-        <h1>{page.title.rendered}</h1>
-      </div>
+      <main className='pageWrapper m-90 t-70 d-50 ld-40'>
+        <h1 className='pageTitle'>{page.title.rendered}</h1>
+        <div
+          className='pageContent'
+          dangerouslySetInnerHTML={{ __html: page.content.rendered }}
+        />
+      </main>
     );
   }
 }
